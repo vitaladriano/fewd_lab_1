98b9177729c650927be09b90d1e800d5aa703d8e
vitaladriano <adriano.mirandav@hotmail.com>
2020-02-03 15:30:52 +0000

    commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var firstName = "Adriano";
+var favouriteColor = "Black";
+var favouriteFood = "pasta";
+var city = "Petropolis";
+
+//the code displays the following variables
+//console.log(firstName+ " "+favouriteColor+" "+favouriteFood+" "+city);
+
+var phrase = "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
+var endingPhrase = "is what we see. Rainbow colours, are made with light bending you see!";
+var one = "Red";
+var two = "orange";
+var three = "yellow";
+var four = "green";
+var five = "blue";
+var six = "indigo";
+var seven = "violet";
+console.log(phrase+"  "+one+ ", "+two+ ", "+three+ ", "+four+ ", "+five+ ", "+six+ ", "+seven+ " " +endingPhrase);
